299c191cfe0207c5aa25386c15bdd9c3e7e528cc
Udara ranasinghe <indi79@hotmail.com>
2020-07-06 05:12:51 +0930

    Updates

---
+++ precache-manifest.21bacae2fcc68c3b5d2e631ca18f9b41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29ebeb951ebbf5f188cea96fb7c59bb",
+    "url": "/react_employee_directory/index.html"
+  },
+  {
+    "revision": "f3f86e0e9d1aba5254cc",
+    "url": "/react_employee_directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a99a5eea9dfa815a85fc",
+    "url": "/react_employee_directory/static/js/2.8e1b47c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_employee_directory/static/js/2.8e1b47c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f86e0e9d1aba5254cc",
+    "url": "/react_employee_directory/static/js/main.537ab018.chunk.js"
+  },
+  {
+    "revision": "43c04402ac085e203aa5",
+    "url": "/react_employee_directory/static/js/runtime-main.7c8d85a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_employee_directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
